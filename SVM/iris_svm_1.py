faf1497053100217acc8b94f76c270024b332ef6
fm you <43927048+pippakiwi@users.noreply.github.com>
2020-03-26 11:29:00 +0800

    Add files via upload

--- SVM/iris_svm_1.py
+++ SVM/iris_svm_1.py
@@ -2,10 +2,18 @@ import sys
 path = 'C://User//youfu//Desktop//machine_learning//SVM//libsvm-3.23//python'
 sys.path.append(path)
 from svmutil import *
-y,x = svm_read_problem("preprocessed_iris_scale.txt")
-model = svm_train(y,x,"-t 3")
-p_label, p_acc, p_val = svm_predict(y,x,model)
-# print(p_label)
-# print(p_acc)
-# print(p_val)
+from sklearn.model_selection import train_test_split
+
+y,x = svm_read_problem("preprocessed_iris.txt")
+acc = []
+
+for i in range(20):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+    model = svm_train(y_train,x_train,'-t 2 -c 32 -g 0.0078')
+    p_label, p_acc, p_val = svm_predict(y_test,x_test,model)
+    acc.append(max(p_acc))
+
+acc_sum = sum(acc)
+acc_average = acc_sum / len(acc)
+print(acc_average)
 
--- SVM/svm_wine.py
+++ SVM/svm_wine.py
@@ -2,10 +2,23 @@ import sys
 path = 'C://User//youfu//Desktop//machine_learning//SVM//libsvm-3.23//python'
 sys.path.append(path)
 from svmutil import *
-y,x = svm_read_problem("wine.txt")
-model = svm_train(y,x)
-p_label, p_acc, p_val = svm_predict(y,x,model)
-# print(p_label)
-# print(p_acc)
-# print(p_val)
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+# import pandas as pd
+# from sklearn.manifold import TSNE
+# from sklearn.preprocessing import scale
+
+class_1_number = 59
+class_2_number = 71
+y,x = svm_read_problem("wine_scale.txt")
+acc = []
+for i in range(20):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+    model = svm_train(y_train,x_train,"-t 1")
+    p_label, p_acc, p_val = svm_predict(y_test,x_test,model)
+    acc.append(max(p_acc))
+print(acc)
+acc_sum = sum(acc)
+acc_average = acc_sum / len(acc)
+print(acc_average)
 
